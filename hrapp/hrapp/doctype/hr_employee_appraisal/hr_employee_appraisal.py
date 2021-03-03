68bedd4a8313b6840114d62e091084534d3e0610
Youssef <youssefrestom@gmail.com>
2021-03-03 08:25:36 +0000

    get items from template on insert HR Employee Appraisal

--- hrapp/hrapp/doctype/hr_employee_appraisal/hr_employee_appraisal.py
+++ hrapp/hrapp/doctype/hr_employee_appraisal/hr_employee_appraisal.py
@@ -3,8 +3,20 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
+
 class HREmployeeAppraisal(Document):
-	pass
+    def before_insert(self):
+        if not self.template:
+            return
+        template = frappe.get_doc("HR Appraisal Template", self.template)
+        for el in template.jobs:
+            row = self.append('jobs', {})
+            row.title = el.title
+            row.description = el.description
+        for el in template.performances:
+            row = self.append('performances', {})
+            row.title = el.title
+            row.description = el.description
