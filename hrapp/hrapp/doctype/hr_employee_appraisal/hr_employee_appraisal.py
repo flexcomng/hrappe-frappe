c7a3dd297f65775d2e72ab7291b678b9a9367711
Youssef <youssefrestom@gmail.com>
2021-03-03 08:02:43 +0000

    feat: update doctypes

--- hrapp/hrapp/doctype/hr_employee_appraisal/hr_employee_appraisal.py
+++ hrapp/hrapp/doctype/hr_employee_appraisal/hr_employee_appraisal.py
@@ -6,5 +6,5 @@ from __future__ import unicode_literals
 # import frappe
 from frappe.model.document import Document
 
-class HRAppraisalDetails(Document):
+class HREmployeeAppraisal(Document):
 	pass
