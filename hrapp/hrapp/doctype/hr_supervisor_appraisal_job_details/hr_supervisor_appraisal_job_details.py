2cf76e7550162555dcfbe802297d77ff4ddb9a14
Youssef <youssefrestom@gmail.com>
2021-03-04 09:23:58 +0000

    init  Supervisor Appraisal doctypes

--- hrapp/hrapp/doctype/hr_supervisor_appraisal_job_details/hr_supervisor_appraisal_job_details.py
+++ hrapp/hrapp/doctype/hr_supervisor_appraisal_job_details/hr_supervisor_appraisal_job_details.py
@@ -6,5 +6,5 @@ from __future__ import unicode_literals
 # import frappe
 from frappe.model.document import Document
 
-class HRAppraisalJobDetails(Document):
+class HRSupervisorAppraisalJobDetails(Document):
 	pass
