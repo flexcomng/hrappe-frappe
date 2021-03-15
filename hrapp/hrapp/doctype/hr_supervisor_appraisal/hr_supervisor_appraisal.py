12a95f10cb962073da3a21a7a50537e7ec2ee2be
Youssef <youssefrestom@gmail.com>
2021-03-15 13:24:23 +0000

    feat: calculate totals in HR Supervisor Appraisal

--- hrapp/hrapp/doctype/hr_supervisor_appraisal/hr_supervisor_appraisal.py
+++ hrapp/hrapp/doctype/hr_supervisor_appraisal/hr_supervisor_appraisal.py
@@ -5,6 +5,29 @@
 from __future__ import unicode_literals
 # import frappe
 from frappe.model.document import Document
+from frappe.utils import flt
+
 
 class HRSupervisorAppraisal(Document):
-	pass
+    def validate(self):
+        self.set_totals()
+
+    def set_totals(self):
+        jobs_count = len(self.jobs)
+        self.job_total = 0
+
+        if jobs_count > 0:
+            for row in self.jobs:
+                self.job_total += flt(row.employee_rating)
+            self.job_average = self.job_total / jobs_count
+
+        performances_count = len(self.performances)
+        self.performance_total = 0
+        if performances_count > 0:
+            for elm in self.performances:
+                self.performance_total += flt(elm.employee_rating)
+            self.performance_average = self.performance_total / performances_count
+
+        self.total = self.job_total + self.performance_total
+        self.average = (
+            self.job_average + self.performance_average) / 2
